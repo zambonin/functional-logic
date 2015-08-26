1d57b6ecb21a2df1606e65d079f284ddf0e3c727
Gustavo Zambonin <gzmbnn@gmail.com>
2015-08-25 21:41:02 -0300

    added prolog rules and pertinent commentary

--- t1a.pl
+++ t1a.pl
@@ -9,14 +9,14 @@ fase(f6).
 fase(f7).
 fase(f8).
 
-% primeira fase
+%% primeira fase
 materia(eel5105, "Circuitos e Técnicas Digitais", f1).
 materia(ine5401, "Introdução à Computação", f1).
 materia(ine5402, "Programação Orientada a Objetos I", f1).
 materia(ine5403, "Fundamentos de Matemática Discreta para Computação", f1).
 materia(mtm5161, "Cálculo A", f1).
 
-% segunda fase
+%% segunda fase
 materia(ine5404, "Programação Orientada a Objetos II", f2).
 materia(ine5405, "Probabilidade e Estatística", f2).
 materia(ine5406, "Sistemas Digitais", f2).
@@ -24,14 +24,14 @@ materia(ine5407, "Ciência, Tecnologia e Sociedade", f2).
 materia(mtm5512, "Geometria Analítica", f2).
 materia(mtm7174, "Cálculo B para Computação", f2).
 
-% terceira fase
+%% terceira fase
 materia(ine5408, "Estruturas de Dados", f3).
 materia(ine5409, "Cálculo Numérico para Computação", f3).
 materia(ine5410, "Programação Concorrente", f3).
 materia(ine5411, "Organização de Computadores I", f3).
 materia(mtm5245, "Álgebra Linear", f3).
 
-% quarta fase
+%% quarta fase
 materia(ine5412, "Sistemas Operacionais I", f4).
 materia(ine5413, "Grafos", f4).
 materia(ine5414, "Redes de Computadores I", f4).
@@ -39,7 +39,7 @@ materia(ine5415, "Teoria da Computação", f4).
 materia(ine5416, "Paradigmas de Programação", f4).
 materia(ine5417, "Engenharia de Software I", f4).
 
-% quinta fase
+%% quinta fase
 materia(ine5418, "Computação Distribuída", f5).
 materia(ine5419, "Engenharia de Software II", f5).
 materia(ine5420, "Computação Gráfica", f5).
@@ -47,7 +47,7 @@ materia(ine5421, "Linguagens Formais e Compiladores", f5).
 materia(ine5422, "Redes de Computadores II", f5).
 materia(ine5423, "Banco de Dados I", f5).
 
-% sexta fase
+%% sexta fase
 materia(ine5424, "Sistemas Operacionais II", f6).
 materia(ine5425, "Modelagem e Simulação", f6).
 materia(ine5426, "Construção de Compiladores", f6).
@@ -55,21 +55,23 @@ materia(ine5427, "Planejamento e Gestão de Projetos", f6).
 materia(ine5430, "Inteligência Artificial", f6).
 materia(ine5453, "Introdução ao Trabalho de Conclusão de Curso", f6).
 
-% sétima fase
+%% sétima fase
 materia(ine5428, "Informática e Sociedade", f7).
 materia(ine5429, "Segurança em Computação", f7).
 materia(ine5431, "Sistemas Multimídia", f7).
 materia(ine5432, "Banco de Dados II", f7).
 materia(ine5433, "Trabalho de Conclusão de Curso I", f7).
 
-% oitava fase
+%% oitava fase
 materia(ine5434, "Trabalho de Conclusão de Curso II", f8).
 
+%% dependências da segunda fase
 depende(ine5404, ine5402).
 depende(ine5405, mtm5161).
 depende(ine5406, eel5105).
 depende(mtm7174, mtm5161).
 
+%% dependências da terceira fase
 depende(ine5408, ine5404).
 depende(ine5409, mtm5512). 
 depende(ine5409, mtm7174).
@@ -77,6 +79,7 @@ depende(ine5410, ine5404).
 depende(ine5411, ine5406).
 depende(mtm5245, mtm5512).
 
+%% dependências da quarta fase
 depende(ine5412, ine5410).
 depende(ine5412, ine5411).
 depende(ine5413, ine5403).
@@ -87,6 +90,7 @@ depende(ine5415, ine5408).
 depende(ine5416, ine5408).
 depende(ine5417, ine5408).
 
+%% dependências da quinta fase
 depende(ine5418, ine5412).
 depende(ine5418, ine5414).
 depende(ine5419, ine5417).
@@ -97,6 +101,7 @@ depende(ine5421, ine5415).
 depende(mtm5245, ine5414).
 depende(ine5423, ine5408).
 
+%% dependências da sexta fase
 depende(ine5424, ine5412).
 depende(ine5425, ine5405).
 depende(ine5426, ine5421).
@@ -105,6 +110,7 @@ depende(ine5430, ine5405).
 depende(ine5430, ine5416).
 depende(ine5453, ine5417).
 
+%% dependências da sétima fase
 depende(ine5428, ine5407).
 depende(ine5429, ine5403).
 depende(ine5429, ine5414).
@@ -112,4 +118,11 @@ depende(ine5431, ine5414).
 depende(ine5432, ine5423).
 depende(ine5433, ine5427).
 
+%% dependências da oitava fase
 depende(ine5434, ine5433).
+
+listar_fase(F, C, N) :- materia(C, N, F).
+listar_deps(C, N, F) :- depende(C, D), materia(D, N, F).
+listar_deps(C, N) :- depende(C, D), materia(D, N, F).
+listar_sucs(C, N, F) :- depende(S, C), materia(S, N, F).
+listar_sucs(C, N) :- depende(S, C), materia(S, N, F).
