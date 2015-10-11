280867dc433e180d6d0e08f44509b23a01d7008b
Gustavo Zambonin <gzmbnn@gmail.com>
2015-10-10 22:42:51 -0300

    two thirds of final haskell assignments done

---
+++ haskell/ine5416_t1.hs
@@ -0,0 +1,39 @@
+module Shapes (Shape (Sphere, Cylinder, Cone, Frustum, OblateSpheroid,
+                      ProlateSpheroid),
+               Radius, Height, BaseRadius, UpperBaseRadius, BiggerSemiAxis,
+               SmallerSemiAxis) where
+
+data Shape = Sphere Radius
+           | Cylinder BaseRadius Height
+           | Cone BaseRadius Height
+           | Frustum BaseRadius UpperBaseRadius Height
+           | OblateSpheroid BiggerSemiAxis SmallerSemiAxis
+           | ProlateSpheroid BiggerSemiAxis SmallerSemiAxis
+     deriving Show
+
+type Radius = Float
+type Height = Float
+type BaseRadius = Float
+type UpperBaseRadius = Float
+type BiggerSemiAxis = Float
+type SmallerSemiAxis = Float
+
+ecc :: BiggerSemiAxis -> SmallerSemiAxis -> Float
+ecc a b = (sqrt(a^2 - b^2))/a
+
+area :: Shape -> Float
+area (Sphere r) = 4*pi*r^2
+area (Cylinder r h) = 2*pi*r*(r+h)
+area (Cone r h) = pi*r*(sqrt(r^2 + h^2) + r)
+area (Frustum r1 r2 h) = pi*(r1^2 + r2^2 + (r1+r2)*sqrt(h^2 + (r1 - r2)^2))
+area (OblateSpheroid a b) = 2*pi*a^2 + ((b^2)/(ecc a b))*log((1 + ecc a b)/
+                                                             (1 - ecc a b))
+area (ProlateSpheroid a b) = 2*pi*(b^2 + ((a*b)/(ecc a b))*(asin(ecc a b)))
+
+volume :: Shape -> Float
+volume (Sphere r) = (4/3)*pi*r^3
+volume (Cylinder r h) = pi*h*r^2
+volume (Cone r h) = (1/3)*pi*h*r^2
+volume (Frustum r1 r2 h) = (1/3)*pi*h*(r1^2 + r2^2 + r1*r2)
+volume (OblateSpheroid a b) = (4/3)*pi*b*a^2
+volume (ProlateSpheroid a b) = (4/3)*pi*a*b^2
---
+++ haskell/ine5416_t2.hs
@@ -0,0 +1,18 @@
+module Summation (oddIntegersSeries, sumOddIntegers, evenIntegersSeries,
+                  sumEvenIntegers, squaresSeries, sumSquares, oddSquaresSeries,
+                  sumOddSquares, almostTwo, almostEuler) where
+
+oddIntegersSeries n = [x*((2*x-1) + 1)/2 | x <- [1..n]]
+sumOddIntegers n = last(oddIntegersSeries n)
+
+evenIntegersSeries n = [x*(2*x + 2)/2 | x <- [1..n]]
+sumEvenIntegers n = last(evenIntegersSeries n)
+
+squaresSeries n = [x*(x+1)*(2*x+1)/6 | x <- [1..n]]
+sumSquares n = last(squaresSeries n)
+
+oddSquaresSeries n = [x*(4*x^2-1)/3 | x <- [1..n]]
+sumOddSquares n = last(oddSquaresSeries n)
+
+almostTwo n = sum([2/(x+x^2) | x <- take n [1..]])
+almostEuler n = 1 + sum([1 / product[1..x] | x <- take n [1..]])
